d9955ce9bb54989a2b46ae43e0e1cd70480f0287
Steve_Loic <tchoumisteve@gmail.com>
2026-01-15 11:58:36 +0100

    add function to the app

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kendo Kaffeehaus
+Diese Anwendung demonstriert die Nutzung von eigenständigen Komponenten zusammen mit der Kendo UI-Komponentenbibliothek. Außerdem wird die Steuerung des Programmablaufs im Template gezeigt (checkout-products.component.html).
+
+## Start der Anwendung
+Starten Sie die Anwendung, indem Sie zuerst npm run json-server und anschließend npm start ausführen.
+
+## Verwendete Kendo UI-Komponenten:
+- Navigation (AppBar) – für die Seiten-Navigation
+- Card – zur Darstellung der Produkte
+- Tooltip - für zusätzliche Informationen auf den Produktkarten
---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,39 @@
+import {Component, inject} from '@angular/core';
+import {CurrencyPipe} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {CardModule} from "@progress/kendo-angular-layout";
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { SkeletonCardComponent } from '../../skeleton-card/skeleton-card.component';
+
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: string;
+  region: string;
+  weight: string;
+  flavor_profile: string;
+  grind_option: string;
+  roast_level: string;
+  image_url: string;
+};
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [
+    CardModule,
+    CurrencyPipe,
+    IndicatorsModule,
+    SkeletonCardComponent,
+  ],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+  http = inject(HttpClient);
+  products = toSignal(
+    this.http.get<Product[]>('http://localhost:3000/coffee'),
+  );
+}
